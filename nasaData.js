0cbfa49283b11558d1ea8ea0df620779acf5ff7f
MyMac <ben.marcantel@gmail.com>
2018-04-22 17:16:41 -0500

    added demo key for hosting

--- nasaData.js
+++ nasaData.js
@@ -1,14 +1,14 @@
 "use strict";
 
 const $ = require('jquery');
-const apiKey = require('./apiKey/apiKey');
+// const apiKey = require('./apiKey/apiKey');
 
 
 
 module.exports.getData = (startDate,endDate) => {
     return new Promise( (resolve, reject) => {
       $.ajax({
-        url: `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${apiKey}`
+        url: `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=DEMO_KEY`
       }).done(data => {
         resolve(data.near_earth_objects);
       }).fail( (error) => reject(error));
